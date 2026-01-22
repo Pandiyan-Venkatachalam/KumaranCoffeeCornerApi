1b00863f475da1c1eaa4e12805bf9f340c8bdd02
Pandiyan V <131842924+Pandiyan-Venkatachalam@users.noreply.github.com>
2026-01-22 21:54:48 +0530

    Rename AppDbContext.cs to ApplicationDbContext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -3,11 +3,12 @@ using KumaranCoffeeCorner.Models;
 
 namespace KumaranCoffeeCorner.Data
 {
-    public class AppDbContext : DbContext
+    public class ApplicationDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
 
         public DbSet<Menu> Menus { get; set; }
         public DbSet<Branch> Branches { get; set; }
     }
+
 }
